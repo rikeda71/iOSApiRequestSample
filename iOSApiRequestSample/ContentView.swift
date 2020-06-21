a5c8eba21c11fb9a9ce84efcb3a7e3b1c0fbf321
Ryuya Ikeda <rikeda71@gmail.com>
2020-06-21 17:00:14 +0900

    Initial Commit

---
+++ iOSApiRequestSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOSApiRequestSample
+//
+//  Created by 池田流弥 on 2020/06/21.
+//  Copyright © 2020 ryuyiked. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
